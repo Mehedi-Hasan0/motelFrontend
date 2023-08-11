65dc72a7a7204d5721112ff16c8766917236b00d
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-08-11 22:01:55 +0600

    Feat: charts added

--- src/components/dashboard/DashboardMenu.jsx
+++ src/components/dashboard/DashboardMenu.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const navItem = [
   { name: "Overview", id: 1, to: "/overview=true" },
+  { name: "Active booking", id: 3, to: "/orders=true" },
   { name: "Host house", id: 2, to: "/renthouse=true" },
-  { name: "Orders", id: 3, to: "/orders=true" },
   { name: "Add categories", id: 4, to: "/addcategories=true" },
 ];
 
