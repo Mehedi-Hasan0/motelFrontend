30bf8ebc8ad99a63a21a38e20a5292ca5bfd452e
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-08-26 20:40:35 +0600

    Fix: Amenities make dynamic stuctured card style and svg's size description added

--- src/Pages/ListingHouseStepTwo/Description.jsx
+++ src/Pages/ListingHouseStepTwo/Description.jsx
@@ -1,5 +1,34 @@
+import StructureCard from "../../components/listingHouse/StructureCard";
+import { LiaShoePrintsSolid } from "react-icons/lia";
+
 const Description = () => {
-  return <div>Describe your apartment</div>;
+  return (
+    <div className=" flex flex-col gap-10 max-w-screen-md mx-auto my-6">
+      <div>
+        <h1 className=" text-[#222222] text-[32px] font-medium">
+          Next, let&apos;s describe your apartment
+        </h1>
+        <p className="text-lg text-[#717171]">
+          Choose up to 2 highlights. We&apos;ll use these to get your
+          description started.
+        </p>
+      </div>
+      <div className=" flex flex-wrap gap-3">
+        <StructureCard
+          style={descriptionCardStyle}
+          Img={LiaShoePrintsSolid}
+          name={"Peaceful"}
+          onClick={handleStoreCardData}
+          storedCardData={storedCardData}
+          svgSize={svgSize}
+        />
+      </div>
+    </div>
+  );
 };
+
+// styles for STructuredCard component
+const descriptionCardStyle =
+  "flex flex-row items-center gap-1 px-6 py-3 rounded-full transition duration-300 cursor-pointer justify-center";
 
 export default Description;
