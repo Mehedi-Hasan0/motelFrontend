d2e8e3d5867b29bc4d5f3b6cead1c83c44313c2e
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-08-27 19:00:16 +0600

    Feat: Preview card full view modal added

--- src/components/listingHouse/PreviewCard.jsx
+++ src/components/listingHouse/PreviewCard.jsx
@@ -7,31 +7,93 @@ const PreviewCard = () => {
     (state) => state.house.currentListingHouse
   );
   return (
-    <div className=" flex flex-col gap-3 rounded-2xl shadow-lg bg-white border-[#f1f1f1] border max-w-sm px-3 py-4">
-      <div className=" relative ">
-        <img
-          src={currentHouseData?.photos[0]}
-          alt="Beautiful house for hositng"
-          className=" aspect-square object-cover rounded-xl"
-        />
-        <p className=" text-sm text-[#222222] px-3 py-1 rounded-md bg-white absolute top-3 left-3">
-          Show preview
-        </p>
-      </div>
-      <div className=" flex flex-row justify-between relative">
-        <span className=" text-sm text-[#222222]">
-          <p className=" font-medium">{currentHouseData?.title}</p>
-          <span className=" flex flex-row gap-1">
-            <p className=" font-bold">${currentHouseData?.basePrice}</p>
-            <span>night</span>
+    <>
+      <div
+        className=" flex flex-col gap-3 rounded-2xl shadow-lg bg-white border-[#f1f1f1] border max-w-sm px-3 py-4 cursor-pointer"
+        onClick={() => window.my_modal_4.showModal()}
+      >
+        <div className=" relative ">
+          {currentHouseData?.photos[0] ? (
+            <img
+              src={currentHouseData?.photos[0]}
+              alt="Beautiful house for hositng"
+              className=" aspect-square object-cover rounded-xl"
+            />
+          ) : (
+            <div className=" bg-amber-200 blur-md opacity-30 aspect-square rounded-xl">
+              {" "}
+            </div>
+          )}
+          <p className=" text-sm text-[#222222] px-3 py-1 rounded-md bg-white absolute top-3 left-3">
+            Show preview
+          </p>
+        </div>
+        <div className=" flex flex-row justify-between relative">
+          <span className=" text-sm text-[#222222]">
+            <p className=" font-medium">{currentHouseData?.title}</p>
+            <span className=" flex flex-row gap-1">
+              <p className=" font-bold">${currentHouseData?.basePrice}</p>
+              <span>night</span>
+            </span>
           </span>
-        </span>
-        <span className=" flex flex-row items-center gap-1 text-sm text-[#222222] absolute top-0 right-0">
-          New
-          <AiFillStar size={16} />
-        </span>
+          <span className=" flex flex-row items-center gap-1 text-sm text-[#222222] absolute top-0 right-0">
+            New
+            <AiFillStar size={16} />
+          </span>
+        </div>
       </div>
-    </div>
+      {/* modal data for Preview Card */}
+      <dialog id="my_modal_4" className="modal">
+        <form method="dialog" className="modal-box w-11/12 max-w-5xl">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+          <h3 className="font-bold text-lg text-center">Full preview</h3>
+          <div className=" grid grid-cols-2 gap-10 overflow-y-auto py-7">
+            <div>
+              <img
+                src={currentHouseData?.photos[0]}
+                alt="Houses"
+                className=" max-w-md rounded-xl object-cover"
+              />
+            </div>
+            <div className=" flex flex-col gap-3">
+              <h6 className=" text-3xl text-[#222222] font-semibold">
+                {currentHouseData?.title}
+              </h6>
+              <p className=" text-xl font-medium text-[#222222] mt-3">
+                Entire rental unit is hosted.
+              </p>
+              <p className="text-base text-[#222222]">
+                {currentHouseData?.floorPlan?.guests} guests ·{" "}
+                {currentHouseData?.floorPlan?.bedrooms} bedrooms ·{" "}
+                {currentHouseData?.floorPlan?.beds} bed ·{" "}
+                {currentHouseData?.floorPlan?.bathroomsNumber} bath
+              </p>
+              <hr className=" h-[1px] bg-[#dddddd] my-5" />
+              <p className=" text-base text-[#222222]">
+                Surround yourself with style in this standout space.
+              </p>
+              <hr className=" h-[1px] bg-[#dddddd] my-5" />
+              <p className=" text-base text-[#222222] font-medium mb-5">
+                Location
+              </p>
+              <p>
+                {currentHouseData?.location?.addressLineOne
+                  ? currentHouseData?.location?.addressLineOne
+                  : currentHouseData?.location?.addressLineTwo
+                  ? currentHouseData?.location?.addressLineTwo
+                  : currentHouseData?.location?.country?.name}
+              </p>
+              <p className=" text-xs text-[#222222] opacity-60">
+                We’ll only share your address with guests who are booked as
+                outlined in our privacy policy.
+              </p>
+            </div>
+          </div>
+        </form>
+      </dialog>
+    </>
   );
 };
 
