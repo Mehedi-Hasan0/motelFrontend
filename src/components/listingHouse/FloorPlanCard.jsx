f1eeb78e2bda471f483c87155afe1c2ac9304933
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-08-28 21:50:40 +0600

    Feat: filter options dropdown added

--- src/components/listingHouse/FloorPlanCard.jsx
+++ src/components/listingHouse/FloorPlanCard.jsx
@@ -2,27 +2,28 @@
 import { BiPlus } from "react-icons/bi";
 import { BiMinus } from "react-icons/bi";
 
-const FloorPlanCard = ({ name, number, setNumber }) => {
+const FloorPlanCard = ({ name, number, setNumber, filter }) => {
   return (
     <div className=" flex flex-row justify-between items-center gap-10 text-[#222222]">
-      <p className=" text-lg ">{name}</p>
+      <p className={` ${filter ? " text-sm" : "text-lg"}`}>{name}</p>
       <div className=" flex flex-row gap-2 items-center text-base">
         <div
           className={` p-2 rounded-full border border-[#dddddd] hover:border-[#717171] transition duration-200
           ${
-            name === "Beds" && number < 2
+            name === "Beds" && !filter && number < 2
               ? " opacity-40 cursor-not-allowed"
               : "cursor-pointer"
           }
           `}
           onClick={() => {
-            if (number === 0 || (name === "Beds" && number < 2)) return;
+            if (number === 0 || (name === "Beds" && number < 2 && !filter))
+              return;
             if (number > 0) {
               setNumber((prev) => prev - 1);
             }
           }}
         >
-          <BiMinus size={20} />
+          <BiMinus size={`${filter ? 14 : 20}`} />
         </div>
         <p className=" w-[20px] text-center">{number}</p>
         <div
@@ -31,7 +32,7 @@ const FloorPlanCard = ({ name, number, setNumber }) => {
             setNumber((prev) => prev + 1);
           }}
         >
-          <BiPlus size={20} />
+          <BiPlus size={`${filter ? 14 : 20}`} />
         </div>
       </div>
     </div>
