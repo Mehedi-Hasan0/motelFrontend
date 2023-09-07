7b705e281367b4450d806c2130d541f39085a026
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-09-07 11:18:24 +0600

    Feat: Home page category section ui is created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,17 @@ import Category from "../components/Home/Category";
 
 const Home = () => {
   return (
-    <div className=" py-10 grid grid-cols-12">
-      <div className=" col-span-9">
-        <Category />
-      </div>
-      <div className=" col-span-3"> </div>
-    </div>
+    <main className="max-w-screen-2xl xl:px-10 mx-auto">
+      <section className=" py-10 grid grid-cols-12 gap-5">
+        <Category styleGrid={"col-span-9"} />
+        <div className=" col-span-3 border-[#e2e2e2] border rounded-xl h-14 flex justify-around items-center">
+          <p className=" text-xs text-[#222222] font-medium">
+            Display total before taxes
+          </p>
+          <input type="checkbox" className="toggle" />
+        </div>
+      </section>
+    </main>
   );
 };
 
