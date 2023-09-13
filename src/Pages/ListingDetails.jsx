da265a1f5984ea89c9db9c99fa39264639223212
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-09-13 11:38:42 +0600

    Feat: Listing photo section added

--- src/Pages/ListingDetails.jsx
+++ src/Pages/ListingDetails.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import ListingTitle from "../components/ListingDetails/ListingTitle";
+import ListingsPhotos from "../components/ListingDetails/ListingsPhotos";
 
 const ListingDetails = () => {
   const listingData = useLoaderData();
@@ -7,7 +8,10 @@ const ListingDetails = () => {
   return (
     <main className="max-w-screen-xl xl:px-10 mx-auto py-7">
       <section className=" flex flex-col gap-7">
+        {/* listing title & wishlist */}
         <ListingTitle listingData={listingData} />
+        {/* listing photos */}
+        <ListingsPhotos listingData={listingData} />
       </section>
     </main>
   );
