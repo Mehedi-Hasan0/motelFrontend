bb7d4fce63a2a5d9de7605aead43561a98b83622
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-09-26 11:29:58 +0600

    Feat: booking page ui is complete

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -1,15 +1,74 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MdKeyboardArrowLeft } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import Payment from "../components/Booking/Payment";
 import Listing from "../components/Booking/Listing";
+import { API } from "../backend";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { PropagateLoader } from "react-spinners";
+import { useDispatch } from "react-redux";
+import { getOneListingRoomsDetails } from "../redux/actions/houseActions";
 
 const Book = () => {
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [searchParams] = useSearchParams();
+
+  //   making the search params in an obj and store in a vairable
+  const searchParamsObj = Object.fromEntries([...searchParams]);
+
   const navigate = useNavigate();
+
+  const params = useParams();
+  const listingId = params?.id;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getOneListingRoomsDetails(listingId));
+
+    const delayLoading = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(delayLoading);
+    };
+  }, [listingId, dispatch, setIsLoading]);
+
+  // stripe provider for elements component like paymaent component
+  useEffect(() => {
+    fetch(`${API}reservations/config`).then(async (r) => {
+      const { publishableKey } = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API}reservations/create_payment_intent`, {
+      method: "POST",
+      body: JSON.stringify({}),
+    }).then(async (r) => {
+      const { clientSecret } = await r.json();
+      setClientSecret(clientSecret);
+    });
+  }, []);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className=" flex justify-center items-center w-full h-[50dvh]">
+        <PropagateLoader color="#565656" speedMultiplier={0.8} />
+      </div>
+    );
+  }
+
   return (
     <main className=" max-w-screen-2xl xl:px-12 mx-auto py-7 xl:py-20">
       <div className=" flex flex-row gap-3 items-center">
@@ -28,9 +87,13 @@ const Book = () => {
       {/* reservations data */}
       <section className=" grid grid-cols-2 gap-20 pt-10 px-10">
         {/* left side data => reservations data */}
-        <Payment />
+        {stripePromise && clientSecret && (
+          <Elements stripe={stripePromise} options={{ clientSecret }}>
+            <Payment searchParamsObj={searchParamsObj} />
+          </Elements>
+        )}
         {/* right side data => listing details */}
-        <Listing />
+        <Listing searchParamsObj={searchParamsObj} />
       </section>
     </main>
   );
--- src/components/Booking/Payment.jsx
+++ src/components/Booking/Payment.jsx
@@ -1,12 +1,8 @@
-import { useSearchParams } from "react-router-dom";
+/* eslint-disable react/prop-types */
 import { useDateFormatting } from "../../hooks/useDateFormatting";
+import { PaymentElement } from "@stripe/react-stripe-js";
 
-const Payment = () => {
-  const [searchParams] = useSearchParams();
-
-  //   making the search params in an obj and store in a vairable
-  const searchParamsObj = Object.fromEntries([...searchParams]);
-
+const Payment = ({ searchParamsObj }) => {
   //   geting the checkin and checkout dates
   const dateObj = {
     checkin: searchParamsObj?.checkin,
@@ -15,15 +11,16 @@ const Payment = () => {
 
   //   dates
   const formattedDates = useDateFormatting(dateObj);
-  //   guests
+  //   reservation data
   const guestNumber = searchParamsObj?.numberOfGuests;
-
-  console.log(formattedDates);
+  const checkin = searchParamsObj.checkin;
+  const checkout = searchParamsObj?.checkout;
+  const orderId = searchParamsObj?.orderId;
 
   return (
     <div>
       {/* trips section */}
-      <div className=" flex flex-col gap-5">
+      <div className=" flex flex-col gap-6">
         <h5 className=" text-[22px] text-[#222222] font-medium">Your trip</h5>
         {/* dates */}
         <div className=" flex flex-row justify-between">
@@ -47,7 +44,41 @@ const Payment = () => {
             Edit
           </p>
         </div>
-        <hr className="w-full h-[1.3px] bg-[#dddddd] mt-5 mb-10" />
+        <hr className="w-full h-[1.3px] bg-[#dddddd] my-4" />
+        {/* payment element */}
+        <form>
+          <h5 className=" text-[22px] text-[#222222] font-medium pb-4">
+            Pay with
+          </h5>
+          <PaymentElement />
+          <hr className="w-full h-[1.3px] bg-[#dddddd] my-10" />
+          <div>
+            <h5 className=" text-[22px] text-[#222222] font-medium">
+              Ground rules
+            </h5>
+            <p className="text-base text-[#222222] py-4">
+              We ask every guest to remember a few simple things about what
+              makes a great guest.
+            </p>
+            <ul className=" list-disc pl-5">
+              <li>Follow the house rules </li>
+              <li>Treat your Host’s home like your own</li>
+            </ul>
+          </div>
+          <hr className="w-full h-[1.3px] bg-[#dddddd] my-10" />
+          <p className="text-xs opacity-70">
+            By selecting the button below, I agree to the Host&apos;s House
+            Rules, Ground rules for guests, Motel&apos;s Rebooking and Refund
+            Policy, and that Motel can charge my payment method if I’m
+            responsible for damage.
+          </p>
+
+          <input
+            type="submit"
+            value="Confirm and pay"
+            className=" mt-7 px-5 py-3 rounded-md bg-[#ff385c] hover:bg-[#d90b63] transition duration-200 ease-in text-white font-medium cursor-pointer "
+          />
+        </form>
       </div>
     </div>
   );
