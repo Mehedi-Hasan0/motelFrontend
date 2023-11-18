163eb675c14975dc6806092d3d743b6fc87f6d1c
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-11-18 22:13:36 +0600

    removed unnecessary div tag

--- src/Pages/MotelYourHome.jsx
+++ src/Pages/MotelYourHome.jsx
@@ -10,9 +10,7 @@ const MotelYourHome = () => {
   return (
     <main className=" mt-8 md:mt-20 xl:px-20 xl:mx-auto max-w-screen-xl px-8 md:px-12">
       <section className=" grid grid-cols-1 lg:grid-cols-2 gap-7 pb-10 items-center">
-        <div className="">
-          <HouseHostingDetails setLatAndLong={setLatAndLong} />
-        </div>
+        <HouseHostingDetails setLatAndLong={setLatAndLong} />
         {/* map section */}
         <div className=" w-full h-[300px] md:h-[400px] lg:h-[450px] mx-auto">
           <Map latAndLong={latAndLong} key="motelMap" />
