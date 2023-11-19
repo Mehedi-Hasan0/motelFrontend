62b215596e230cccb0cd70cf8a0fac5094407817
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-11-19 11:24:35 +0600

    Feat: list house step one pages responsiveness added

--- src/Pages/ListingHouseStepOne/ListHouseStepOne.jsx
+++ src/Pages/ListingHouseStepOne/ListHouseStepOne.jsx
@@ -2,13 +2,15 @@ import step1 from "../../assets/video/step1_video.mp4";
 
 const ListHouseStepOne = () => {
   return (
-    <section className=" h-[80vh] flex flex-row justify-around gap-10 items-center w-full">
-      <div className="flex flex-col gap-4 text-[#222222] px-10">
+    <section className=" h-[80vh] flex flex-col lg:flex-row justify-around gap-5 lg:gap-10 items-center w-full my-8 sm:my-12 md:my-16 lg:my-0">
+      <div className="flex flex-col gap-2 md:gap-4 text-[#222222] sm:px-5 md:px-8 lg:px-10">
         <div className=" flex flex-col gap-2 font-medium">
-          <p className=" text-lg">Step 1</p>
-          <h2 className=" text-5xl">Tell us about your place</h2>
+          <p className=" text-base md:text-lg">Step 1</p>
+          <h2 className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+            Tell us about your place
+          </h2>
         </div>
-        <p className=" text-lg">
+        <p className=" text-xs sm:text-sm md:text-base lg:text-lg">
           In this step, we&apos;ll ask you which type of property you have and
           if guests will book the entire place or just a room. Then let us know
           the location and how many guests can stay.
@@ -19,7 +21,7 @@ const ListHouseStepOne = () => {
         autoPlay={true}
         muted
         type="video/mp4"
-        className=" max-w-md"
+        className=" max-w-xs sm:max-w-md"
       />
     </section>
   );
