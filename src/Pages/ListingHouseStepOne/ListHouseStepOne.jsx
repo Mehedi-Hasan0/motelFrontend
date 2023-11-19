de1c0b200e53d52e1e4b7c0e4c6d7a55416fb162
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-11-19 12:01:26 +0600

    Feats: step two overview pages responsiveness added

--- src/Pages/ListingHouseStepOne/ListHouseStepOne.jsx
+++ src/Pages/ListingHouseStepOne/ListHouseStepOne.jsx
@@ -4,7 +4,7 @@ const ListHouseStepOne = () => {
   return (
     <section className=" h-[80vh] flex flex-col lg:flex-row justify-around gap-5 lg:gap-10 items-center w-full my-8 sm:my-12 md:my-16 lg:my-0">
       <div className="flex flex-col gap-2 md:gap-4 text-[#222222] sm:px-5 md:px-8 lg:px-10">
-        <div className=" flex flex-col gap-2 font-medium">
+        <div className=" flex flex-col gap-2 font-medium min-h-[130px]">
           <p className=" text-base md:text-lg">Step 1</p>
           <h2 className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
             Tell us about your place
