1dd19a61023be46dab85b0b1bfd384aa18cf3b4e
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-11-19 12:27:57 +0600

    final step overview pages made responsive

--- src/Pages/ListingHouseFinalStep/FinalStepOverview.jsx
+++ src/Pages/ListingHouseFinalStep/FinalStepOverview.jsx
@@ -2,13 +2,15 @@ import video from "../../assets/video/step_3.mp4";
 
 const FinalStepOverview = () => {
   return (
-    <section className=" h-[80vh] flex flex-row justify-around gap-10 items-center w-full">
-      <div className="flex flex-col gap-4 text-[#222222] px-10">
+    <section className=" h-[80vh] flex flex-col lg:flex-row justify-evenly gap-5 lg:gap-10 items-center w-full my-8 sm:my-12 md:my-16 lg:my-0">
+      <div className="flex flex-col gap-2 md:gap-4 text-[#222222] sm:px-5 md:px-8 lg:px-10">
         <div className=" flex flex-col gap-2 font-medium min-h-[100px]">
-          <p className=" text-lg mb-2">Step 3</p>
-          <h2 className=" text-5xl">Finish up and publish</h2>
+          <p className=" text-base md:text-lg mb-2">Step 3</p>
+          <h2 className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+            Finish up and publish
+          </h2>
         </div>
-        <p className=" text-lg">
+        <p className=" text-xs sm:text-sm md:text-base lg:text-lg">
           Finally, you’ll choose if you&apos;d like to start with an experienced
           guest, then you&apos;ll set your nightly price. Answer a few quick
           questions and publish when you&apos;re ready.
@@ -19,7 +21,7 @@ const FinalStepOverview = () => {
         autoPlay={true}
         muted
         type="video/mp4"
-        className=" max-w-md"
+        className=" max-w-xs sm:max-w-md"
       />
     </section>
   );
